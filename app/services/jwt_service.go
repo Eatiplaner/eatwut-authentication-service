a66c6fbcc4b714b79e585970f9a4e36565d178b0
Louis <huynguyennbk@gmail.com>
2022-08-06 22:46:08 +0700

    feat: increase expire time access and refresh token

--- app/services/jwt_service.go
+++ app/services/jwt_service.go
@@ -22,10 +22,10 @@ type AccessDetails struct {
 
 func CreateToken(userid uint64) (*integrations.JwtToken, error) {
 	td := &integrations.JwtToken{}
-	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AtExpires = time.Now().Add(time.Minute * 30).Unix()
 	td.AccessUuid = uuid.NewV4().String()
 
-	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 30).Unix()
 	td.RefreshUuid = uuid.NewV4().String()
 
 	var err error
