940e8350a9fcacb2e9923a0d9e78da6d7c45cbc7
Louis <huynguyennbk@gmail.com>
2022-08-20 22:28:47 +0700

    feat: using metadata for authorization token instead of request payload

--- app/grpc/server/jwt.go
+++ app/grpc/server/jwt.go
@@ -2,39 +2,50 @@ package server
 
 import (
 	"Eatiplan-Auth/app/grpc/client"
-	"Eatiplan-Auth/app/grpc/rpc_pb"
 	pb "Eatiplan-Auth/app/grpc/rpc_pb"
 	"context"
 	"errors"
 	"log"
 
 	"Eatiplan-Auth/app/services"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type JwtServer struct {
 	pb.JwtServiceServer
 }
 
-func (*JwtServer) ValidToken(ctx context.Context, req *pb.ValidRequest) (*pb.ValidResponse, error) {
+func (*JwtServer) ValidToken(ctx context.Context, req *emptypb.Empty) (*pb.ValidResponse, error) {
 	log.Println("Valid Token is processing...")
-	log.Printf("Token: %s", req.GetToken())
+	var values []string
+	var error error
 
-	token := req.GetToken()
-	error := services.TokenValid(token)
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok {
+		values = md.Get("authorization")
+	}
 
-	// Check User is Active or not after valid token
-	if error == nil {
-		resp, err := client.Service.CheckActivation(&rpc_pb.CheckActivationReq{
-			Token: token,
-		})
+	if len(values) > 0 {
+		token := values[0]
+		log.Printf("Token: %s", token)
+		error = services.TokenValid(token)
 
-		if err != nil {
-			error = err
+		// Check User is Active or not after valid token
+		if error == nil {
+			resp, err := client.Service.CheckActivation(ctx, token)
+
+			if err != nil {
+				error = err
+			}
+
+			if !resp.IsActive {
+				error = errors.New("User has not been activate yet")
+			}
 		}
-
-		if !resp.IsActive {
-			error = errors.New("User has not been activate yet")
-		}
+	} else {
+		error = errors.New("Authorization has not been set yet in metadata")
 	}
 
 	resp := &pb.ValidResponse{
